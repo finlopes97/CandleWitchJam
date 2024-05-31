eb5b0b9c07cd88051a044618ceb8618d94352a31
LONDURGERUS <134016121+LONDURGERUS@users.noreply.github.com>
2024-05-31 07:26:58 -0700

    Tilled part that needs the Tiled Addon
    there is a different file addon that can do internal?

---
+++ Assets/Sprites/TileSets/CanWCU1.tsx
@@ -0,0 +1,174 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="CWCastleA" tilewidth="320" tileheight="320" tilecount="459" columns="27">
+ <editorsettings>
+  <export target="CanWCU1..tsx" format="tsx"/>
+ </editorsettings>
+ <image source="MTV_CastileBrick5.png" width="8640" height="5440"/>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="61">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="38" y="24" width="282" height="296"/>
+  </objectgroup>
+ </tile>
+ <tile id="62">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="24" width="320" height="296"/>
+  </objectgroup>
+ </tile>
+ <tile id="83">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="154" y="19">
+    <polygon points="0,0 0,52 166,52 166,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="84">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="75">
+    <polygon points="-1,-4 319,-4 319,-56 -1,-56"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="85">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="82">
+    <polygon points="0,-11 143,-11 143,-63 0,-63"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="116">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="24" width="299" height="296"/>
+  </objectgroup>
+ </tile>
+ <tile id="124">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="24" width="320" height="296"/>
+  </objectgroup>
+ </tile>
+ <tile id="125">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="24" width="320" height="76"/>
+  </objectgroup>
+ </tile>
+ <tile id="126">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="22" width="320" height="76"/>
+  </objectgroup>
+ </tile>
+ <tile id="137">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="38" y="24" width="282" height="296"/>
+  </objectgroup>
+ </tile>
+ <tile id="138">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="24" width="320" height="296"/>
+  </objectgroup>
+ </tile>
+ <tile id="139">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="24" width="282" height="296"/>
+  </objectgroup>
+ </tile>
+ <tile id="164">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="38" y="0" width="282" height="320"/>
+  </objectgroup>
+ </tile>
+ <tile id="166">
+  <objectgroup draworder="index" id="3">
+   <object id="13" x="0" y="0" width="282" height="320"/>
+  </objectgroup>
+ </tile>
+ <tile id="169">
+  <objectgroup draworder="index" id="4">
+   <object id="3" x="319.909" y="222.273">
+    <polygon points="0.0909091,9.72727 0.0909091,97.7273 -257.909,97.7273"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="170">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="246">
+    <polygon points="0,-14 0,74 320,74 320,-119"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="171">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="120">
+    <polygon points="0,7 320,-96 320,200 0,200"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="191">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="38" y="0" width="282" height="290"/>
+  </objectgroup>
+ </tile>
+ <tile id="192">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="320" height="290"/>
+  </objectgroup>
+ </tile>
+ <tile id="193">
+  <objectgroup draworder="index" id="5">
+   <object id="7" x="0" y="0" width="282" height="290"/>
+  </objectgroup>
+ </tile>
+ <tile id="195">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="24" width="320" height="296"/>
+  </objectgroup>
+ </tile>
+ <tile id="196">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="42">
+    <polygon points="0,-18 62,-42 320,-42 0,278"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="229">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="320">
+    <polygon points="25,0 320,-296 320,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="256">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="-1" y="27">
+    <polygon points="1,-3 26,-27 321,-27 1,293"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="384">
+  <animation>
+   <frame tileid="384" duration="100"/>
+   <frame tileid="383" duration="100"/>
+   <frame tileid="382" duration="100"/>
+   <frame tileid="381" duration="100"/>
+   <frame tileid="380" duration="100"/>
+   <frame tileid="379" duration="100"/>
+   <frame tileid="378" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="411">
+  <animation>
+   <frame tileid="411" duration="100"/>
+   <frame tileid="410" duration="100"/>
+   <frame tileid="409" duration="100"/>
+   <frame tileid="408" duration="100"/>
+   <frame tileid="407" duration="100"/>
+   <frame tileid="406" duration="100"/>
+   <frame tileid="405" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
